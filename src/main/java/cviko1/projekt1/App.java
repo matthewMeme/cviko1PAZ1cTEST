4862b5cfd55e925093896c49ac13f56e3bf54586
student <student@158.197.35.89>
2019-09-17 10:36:00 +0200

    vypocita zlozity priklad

--- src/main/java/cviko1/projekt1/App.java
+++ src/main/java/cviko1/projekt1/App.java
@@ -4,10 +4,13 @@ package cviko1.projekt1;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Čau Európa!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Čau Európa!");
+		int g = 0;
+		for (int i = 0; i < 6; i++) {
+			g += 1;
+		}
+		System.out.println(g);
+	}
 }
